8bbea762e9a21f405ae3010d6267c5ef3ec5613c
Glory Gweth <glorygweth6@gmail.com>
2023-08-21 13:49:32 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci=[]
+    if length <= 2:
+        for n in range(length):
+            fibonacci.append(n)
+    elif length>2:
+        fibonacci=[0,1]
+        while fibonacci.__len__()<length:
+            n=(fibonacci[-1]+fibonacci[-2])
+            fibonacci.append(n)
+    print(fibonacci)
     pass
+print_fibonacci(1)
